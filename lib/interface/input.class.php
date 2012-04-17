d19916e1b22c4a3a90dddfe2b734bd887272dd20
hregnie2 <hregnie2@adm-info03>
2012-04-17 09:28:47 +0200

    Modifying names for Input classes, old one and new one

--- lib/interface/input.class.php
+++ lib/interface/input.class.php
@@ -1,143 +1,102 @@
 <?php
+namespace html;
 
-class Input{
-
-	protected $name; //SQL column name of the field
-	protected $label; //Label to be displayed in the HTML
-	protected $type; //HTML type (text, password, hidden)
-	protected $not_null; //True if the user input can't be null
-	protected $default_value; //Default value
-	protected $error_msg=null; //If it exists, the message will be displayed when an error occurs.
-	protected $regexp=null; //Regular Expression to test the user input
-	protected $attributes=''; //HTML attributes for specific purposes
-	protected $error=false; //If there is an error
-	protected $value=null; //Value passed by user
-	protected $validated=false;
+class Input extends Element{
+	
+	protected $attributes=array();
+	protected $label=null;
+	protected $regexp=null;
 	protected $prefix='';
-
-	public function __construct($name, $label=null, $type='text', $default_value=null, $not_null=false){
-		$this->name=$name;
+	public static protected $authorized_attributes;
+	
+	public function __construct($name,$type,$default_value=null,$label=null){
+		parent::Element('input','');
+		$this->attributes['name']=$name;
+		$this->attributes['type']=$type;
+		if ($default_value !== null){
+			$this->attributes['value']=$default_value;
+		}
 		$this->label=$label;
-		$this->type=$type;
-		$this->not_null=$not_null;
-		$this->default_value=$default_value;
-	}
-
-	public final function setErrorMessage($error_msg){
-		$this->error_msg=$error_msg;
-	}
-
-	/**
-	 * You can set a regular expression to filter the user-input.
-	 */
-	public final function set_regexp($regexp){
-		$this->regexp=$regexp;
-	}
-
-	public final function set_html_attributes($attributes){
-		$this->attributes=$attributes;
-	}
-
-	public final function get_name(){
-		return $this->name;
 	}
 	
-	public final function get_type(){
-		return $this->type;
-	}
-
 	/**
-	 * TODO: We need to do something about displaying user-input like that...
+	 * Set maxlength attribute
+	 * @param integer $value
+	 * @throws \UnexpectedValueException
+	 * @return $this
 	 */
-	public function display(){
-		$this->validate();
-		$string=$this->display_label();
-		$string.='<input name="'.$this->prefix.$this->name.'" id="'.$this->prefix.$this->name.'" type="'.$this->type.'" class="'.$this->name.($this->error?' form_error':'').'" '.$this->attributes; //Minimal html for an input
-		if($this->get_value() !== null){//If there was a user input, display it in the field
-			$string.=' value="'.$this->value.'"';
-		} elseif($this->default_value !== null){//Else, display the default value if it exists
-			$string.=' value="'.$this->default_value.'"';
+	public function setMaxlenght($value){
+		if(!is_integer($value)){
+			throw new \UnexpectedValueException('Unexcepted value for maxlength attribute ('.$value.')');
+		}else{
+			$this->attributes['maxlength']=$value;
+			return $this;
 		}
-		if($this->regexp !== null){//If there was a regexp, use HTML5 pattern to check input
-			$string.=' pattern="'.trim($this->regexp,'/').'"';
+	}
+	
+	/**
+	 * set size attribute
+	 * @param integer $value
+	 * @throws \UnexpectedValueException
+	 * @return $this 
+	 */
+	public function setSize($value){
+		if(!is_integer($value)){
+			throw new \UnexpectedValueException('Unexcepted value for size attribute ('.$value.')');
+		}else{
+			$this->attributes['size']=$value;
+			return $this;
 		}
-		$string.=' />'.$this->display_error();
-		return $string;
 	}
-
+	
 	/**
-	 * return the label tag if there is one
+	 * set a regular expression to validate input
+	 * @param string $value
+	 * @return $this 
 	 */
-	protected function display_label(){
-		if($this->label != null){//If a label is set, display it
-			return '<label for="'.$this->prefix.$this->name.'" class="'.$this->name.'">'.$this->label.'</label>';
-		}
-		return '';
+	public function setRegexp($value){
+		$this->regexp=$value;
+		return $this;
 	}
-
+	
 	/**
-	 * Display the error message if there is one
+	 * set a prefix for name and id
+	 * @param type $value
+	 * @return $this 
 	 */
-	protected function display_error(){
-		if($this->error && $this->error_msg != null){//Add error message if an error was encountered
-			return '<span class="form_error_msg">'.$this->error_msg.'</span>';
-		}
-		return '';
-	}
-
-	/**
-	 * Set the value manually.
-	 */
-	public final function set_value($value){
-		$this->value=$value;
-	}
-
-	/**
-	 * Set the value manually.
-	 */
-	public final function set_prefix($value){
+	public function setPrefix($value){
 		$this->prefix=$value;
+		return $this;
 	}
-
+	
 	/**
-	 * Check and return the value or false if the user_input was incorrect.
-	 */
-	public function get_value($validate=true){
-		if($this->value === null){//If no value was set, take the default one
-			$this->value=$this->default_value;
-			if(isset($_POST[$this->prefix.$this->name])){//If a value is set by the user, take it
-				$this->value=$_POST[$this->prefix.$this->name];
-			}
-		}
-		if(!$this->validated && $validate){
-			$this->validate();
-		}
-		if($this->error){//Return false if there is an error
-			return false;
-		}
-		return $this->value;
-	}
-
-	/**
-	 * Validate the value of the input.
+	 * Validate the input. Try to match the regexp.
+	 * @return boolean
+	 * @throws \InvalidArgumentException 
 	 */
 	public function validate(){
-		if($this->validated){
-			return!$this->error;
-		}
 		if($this->regexp !== null){//If you have set a regexp to test input
 			$result=preg_replace($this->regexp, '', $this->get_value(false), 1); //replace by nothing
 			if($result != ''){//If there is chars left, it means the input doesn't match the regexp...
-				$this->error=true;
+				throw new \InvalidArgumentException('The input does not match the regexp ('.$this->regexp.')');
 			}
 		}
-		if($this->not_null){//If you don't want an empty input
-			if($this->get_value(false) == ''){//check if input is empty
-				$this->error=true;
-			}
-		}
-		$this->validated=true;
-		return!$this->error;
+		return true;
 	}
-
+	
+	public function __ToString(){
+		$tmp = '';
+		if ($this->label !== null){
+			$tmp = '<label for="'.$this->prefix.$this->attributes['id'].'">'.$this->label.'</label>';
+		}
+		$tmp.='<input';
+		foreach ($this->attributes as $attribute){
+			$tmp.=' '.$attribute[0].'="'.$attribute[1].'"';
+		}
+		$tmp.='/>';
+		return $tmp;
+	}
 }
+
+Input::$authorized_attributes=array_merge(parent::$authorized_attributes,array('accept','alt','checked','disabled','maxlength','name','readonly','size','src','type','value','onblur','onchange','onclick','ondblclick','onfocus','onmousedown','onmouseover','onmousemove','onmouseout','onmouseup','onkeydown','onkeypress','onkeyup','onselect'));
+?>
