b67d52442cdbc6a14a0aba1d133681e36a54e441
Vincent Chalnot <vincent.chalnot@gmail.com>
2012-04-19 09:50:53 +0200

    Moving some functions from Core to Utils New function camelize to put sentences in CamelCase

--- lib/core/core.class.php
+++ lib/core/core.class.php
@@ -200,14 +200,14 @@ class Core{
 	/**
 	 * Return an array with all the nodes loaded in memory
 	 */
-	public function get_loaded_nodes(){
+	public function getLoadedNodes(){
 		return $this->nodes;
 	}
 
 	/**
 	 * Check the existence of a node
 	 */
-	public function node_exists($id){
+	public function nodeExists($id){
 		$id = (int)$id;
 		if(isset($this->nodes[$id])){
 			return true;
@@ -349,9 +349,6 @@ class Core{
 		return $this->db->secureString($this->secureText($string));
 	}
 
-	public final function secureText($string){
-		return nl2br(htmlspecialchars($string, ENT_COMPAT, 'UTF-8'));
-	}
 
 	/**
 	 * WARNING this function is not meant to secure user-input for the database, you need to use secureString($string, true); !!!
@@ -362,15 +359,6 @@ class Core{
 		return strip_tags($string, $this->get('safe_tags'));
 	}
 
-	/**
-	 * Secure input to prevent XSS attacks
-	 * This is just converting sensitive characters to their HTML equivalents
-	 * @param (String) original
-	 * @return (String) secured
-	 */
-	public function secureDisplay($string){
-		return htmlspecialchars($string, ENT_COMPAT, 'UTF-8');
-	}
 
 	/**
 	 * Get a global configuration variable from database
@@ -464,45 +452,7 @@ class Core{
 		return $string;
 	}
 
-	/**
-	 * This function tries to gracefully shorten titles and short strings to
-	 * the specified number of characters.
-	 */
-	public function shorten($string, $len=40){
-		$tmp = explode(',', $string);
-		$string = trim($tmp[0]);
-		if(strlen($string) > $len){
-			$tmp = substr($string, 0, $len + 1);
-			$cut = strrpos($tmp, ' ');
-			$tmp = substr($tmp, 0, $cut);
-			if($tmp == ''){
-				$tmp = substr($string, 0, $len);
-			}
-			return trim($tmp).'&hellip;';
-		}
-		return $string;
-	}
-
-	/**
-	 * This function tries to gracefully shorten a long text to the specified
-	 * number of characters.
-	 */
-	public function summarize($string, $len=300){
-		if(strlen($string) > $len){
-			$tmp = substr($string, 0, $len + 1);
-			$cut = strrpos($tmp, '.', -1);
-			$cut2 = strrpos($tmp, ',', -1);
-			if($cut2 > $cut){
-				$cut = $cut2;
-			}
-			$tmp = substr($tmp, 0, $cut);
-			if($tmp == ''){
-				$tmp = substr($string, 0, $len);
-			}
-			return trim($tmp).'&hellip;';
-		}
-		return $string;
-	}
+	
 
 	/**
 	 * You should use a common function to generate URLs to the nodes
@@ -522,24 +472,6 @@ class Core{
 		return $link;
 	}
 
-	public static function slugify($string){
-		$text = self::replaceNonAscii('-', $string); // replace non letter or digits by -
-		$text = trim($text, '-');
-		if(function_exists('iconv')){
-			$text = iconv('UTF-8', 'ASCII//TRANSLIT', $text); // transliterate
-		}
-		$text = strtolower($text);
-		$text = preg_replace('~[^-\w]+~', '', $text); // remove unwanted characters
-		if(empty($text)){
-			return 'n-a';
-		}
-		return $text;
-	}
-
-	public static function replaceNonAscii($subject, $replacement){
-		return preg_replace('~[^\\pL\d]+~u', $replacement, $subject);
-	}
-
 	public function generateTagsLinks(node_generic $node){
 		$str = '';
 		if(!$node->get_auth('read')){
@@ -556,22 +488,6 @@ class Core{
 		return $str;
 	}
 
-	public function convertToText($html, $cut=null){
-		if($cut == null){
-			return html_entity_decode(strip_tags($html));
-		}
-		return $this->summarize(html_entity_decode(strip_tags($html)), $cut);
-	}
-
-	public static final function getAttributesFromArray(array $array){
-		$str = '';
-		foreach($array as $key => $value){
-			if(is_array($value)){
-				$value = implode(' ', $value);
-			}
-			$str.=$key.'="'.$value.'" ';
-		}
-		return $str;
-	}
+	
 
 }
--- lib/core/utils.class.php
+++ lib/core/utils.class.php
@@ -1,12 +1,108 @@
 <?php
-class Utils {
-	static public function camelize($key){
-		$tmp=explode('_',$key);
-		$tmp2='';
-		foreach($tmp as $value){
-			$tmp2.=$value;
+
+class Utils{
+
+	public static function slugify($string, $delimiter = '-'){
+		$text = self::replaceControlChars($string, $delimiter); // replace non letter or digits by the delimiter
+		$text = trim($text, $delimiter);
+		if(function_exists('iconv')){
+			$text = iconv('UTF-8', 'ASCII//TRANSLIT', $text); // transliterate
 		}
-		return $tmp2;
+		$text = strtolower($text);
+		$text = preg_replace('~[^-\w]+~', '', $text); // remove unwanted characters
+		if(empty($text)){
+			return 'n-a';
+		}
+		return $text;
 	}
+
+	public static function replaceControlChars($subject, $replacement){
+		return preg_replace('~[^\\pL\d]+~u', $replacement, $subject);
+	}
+
+	public static function camelize($string){
+		$string = ucwords(self::replaceControlChars($string, ' '));
+		$string = str_replace(' ', '', $string);
+		return $string;
+	}
+
+	public function convertToText($html, $cut = null){
+		if($cut == null){
+			return html_entity_decode(strip_tags($html));
+		}
+		return $this->summarize(html_entity_decode(strip_tags($html)), $cut);
+	}
+
+	public static final function getAttributesFromArray(array $array){
+		$str = '';
+		foreach($array as $key => $value){
+			if(is_array($value)){
+				$value = implode(' ', $value);
+			}
+			$str.=$key.'="'.$value.'" ';
+		}
+		return $str;
+	}
+
+	/**
+	 * This function tries to gracefully shorten titles and short strings to
+	 * the specified number of characters.
+	 */
+	public function shorten($string, $len = 40){
+		$tmp = explode(',', $string);
+		$string = trim($tmp[0]);
+		if(strlen($string) > $len){
+			$tmp = substr($string, 0, $len + 1);
+			$cut = strrpos($tmp, ' ');
+			$tmp = substr($tmp, 0, $cut);
+			if($tmp == ''){
+				$tmp = substr($string, 0, $len);
+			}
+			return trim($tmp).'&hellip;';
+		}
+		return $string;
+	}
+
+	/**
+	 * This function tries to gracefully shorten a long text to the specified
+	 * number of characters.
+	 */
+	public function summarize($string, $len = 300){
+		if(strlen($string) > $len){
+			$tmp = substr($string, 0, $len + 1);
+			$cut = strrpos($tmp, '.', -1);
+			$cut2 = strrpos($tmp, ',', -1);
+			if($cut2 > $cut){
+				$cut = $cut2;
+			}
+			$tmp = substr($tmp, 0, $cut);
+			if($tmp == ''){
+				$tmp = substr($string, 0, $len);
+			}
+			return trim($tmp).'&hellip;';
+		}
+		return $string;
+	}
+
+	/**
+	 * Secure input to prevent XSS attacks
+	 * This is just converting sensitive characters to their HTML equivalents
+	 * @param (String) original
+	 * @return (String) secured
+	 */
+	public function secureDisplay($string){
+		return htmlspecialchars($string, ENT_COMPAT, 'UTF-8');
+	}
+
+	/**
+	 * This is just converting sensitive characters to their HTML equivalents
+	 * @param (String) original
+	 * @return (String) secured
+	 */
+	public final function secureText($string){
+		return nl2br(htmlspecialchars($string, ENT_COMPAT, 'UTF-8'));
+	}
+
 }
+
 ?>
