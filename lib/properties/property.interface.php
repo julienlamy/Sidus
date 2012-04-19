afa11b201bf87a17c6c784d35fe148a0a04cdda1
Vincent Chalnot <vincent.chalnot@gmail.com>
2012-04-19 09:55:05 +0200

    The interface is almost finished, there is still a lot to do in the classes

--- lib/properties/property.interface.php
+++ lib/properties/property.interface.php
@@ -6,24 +6,32 @@ interface propertyInterface{
 
 	protected $value;
 	protected $has_changed = false;
-	protected $type;
+	protected $table_name;
+	protected $column_name;
+	protected $pdo_param;
+	protected $model_name;
+	protected $input;
 	
 	/**
 	 * Set the value for the first time where $value is a correct PHP type
-	 * @param $value
+	 * @see http://php.net/manual/en/pdo.constants.php Documentation for PDO Params
+	 * @param string $table_name Table name of the property in the Database
+	 * @param string $column_name Column name of the property in the Database
+	 * @param integer $pdo_type The PDO param type of the value in the DB
+	 * @param string $model_name If different from column name, the PHP name
 	 */
-	public function __constructor($value);
+	public function __constructor($table_name, $column_name, $pdo_param = \PDO::PARAM_STR, $model_name = null);
 
 	/**
 	 * Return the value for PHP with correct type and no processing for display
-	 * @return mixed
+	 * @return mixed $value
 	 */
 	public function get();
 
 	/**
 	 * Set the value of the property where $value is a correct PHP type
 	 * Must change $this->has_changed to true;
-	 * @param $value
+	 * @param mixed $value
 	 * @return boolean
 	 */
 	public function set($value);
@@ -36,18 +44,15 @@ interface propertyInterface{
 	
 	/**
 	 * Reset the hasChanged property to false
+	 * @return null
 	 */
 	public function reset();
 
 	/**
-	 * Return a string ready to be inserted in a SQL query, be sure to escape
-	 * special characters properly !
-	 * If the database type is not known, there MUST be a fall back with a
-	 * standard SQL92 output.
-	 * @param \SQL\Database $db handler to database 
-	 * @return string
+	 * Return a variable for binding params to prepared statements
+	 * @return mixed $value
 	 */
-	public function toDB(\SQL\Database $db);
+	public function toDB();
 
 	/**
 	 * Check if the $value is acceptable for this property
@@ -57,19 +62,46 @@ interface propertyInterface{
 	public function check($value);
 
 	/**
-	 * Return the SQL type of the property
-	 * If the database type is not known, there MUST be a fall back with a
-	 * standard SQL92 type.
-	 * @param $db_type = mysql | sqlite3 | postgres | oracle ...
-	 * @return string
+	 * The default output formatting of the variable
+	 * @return string $value 
 	 */
-	//public function getSqlType($db_type);
-
+	public function __toString();
+	
 	/**
-	 * Return a primitive PHP type or a class name as a string.
-	 * @return string
+	 * Returns the table name of the property
+	 * @return string $table_name 
 	 */
-	//public function getPhpType();
+	public function getTableName();
+	
+	/**
+	 * Returns the column name of the property
+	 * @return string $column_name 
+	 */
+	public function getColumnName();
+	
+	/**
+	 * Returns the model name of the property, $column_name by default
+	 * @return string $mode_name 
+	 */
+	public function getModelName();
+	
+	/**
+	 * Returns the PDO parameter type of the property
+	 * @return string $value 
+	 */
+	public function getPDOParam();
+	
+	/**
+	 * Returns the default input object for forms for this property
+	 * @return string $value 
+	 */
+	public function getInput();
+	
+	/**
+	 * Set the default input object for forms for this property
+	 * @param \HTML\Input $input 
+	 */
+	public function setInput(\HTML\Input $input);
 	
 }
 
