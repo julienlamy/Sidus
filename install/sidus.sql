83fea12b2953c476239e5c588d460c65e6ee8311
vincent <vincent@spacepc.lan>
2012-04-22 23:43:00 +0200

    Updated model with permissions as a node

--- install/sidus.sql
+++ install/sidus.sql
@@ -1,53 +1,68 @@
-SET storage_engine=InnoDB;
+SET STORAGE_ENGINE = InnoDB;
 SET NAMES 'utf8' COLLATE 'utf8_general_ci';
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS sd_object_type;
+DROP TABLE IF EXISTS sd_allowed_object_type;
+DROP TABLE IF EXISTS sd_forbidden_object_type;
+DROP TABLE IF EXISTS sd_node;
+DROP TABLE IF EXISTS sd_version;
+DROP TABLE IF EXISTS sd_object;
+DROP TABLE IF EXISTS sd_info;
+DROP TABLE IF EXISTS sd_permission;
+DROP TABLE IF EXISTS sd_user;
+DROP TABLE IF EXISTS sd_session;
 
 CREATE TABLE IF NOT EXISTS sd_object_type (
-	id INTEGER unsigned NOT NULL AUTO_INCREMENT,
-	parent_id INTEGER unsigned NOT NULL,
+	object_id BIGINT(20) unsigned NOT NULL,
 
 	type_name VARCHAR(30) NOT NULL,
-	class_name VARCHAR(30) NOT NULL DEFAULT 'Node',
-	class_path VARCHAR(60) NOT NULL DEFAULT 'lib/nodes/class.node_generic.php',
-	description TEXT,
+	class_name VARCHAR(30) NOT NULL DEFAULT '\Sidus\Node',
+	class_path VARCHAR(60) NOT NULL DEFAULT 'lib/nodes/class.simple.php',
 
-	PRIMARY KEY (id),
-	FOREIGN KEY (parent_id) REFERENCES sd_object_type(id)
+	PRIMARY KEY (object_id),
+	FOREIGN KEY (object_id) REFERENCES sd_object(id)
 );
 
 CREATE TABLE IF NOT EXISTS sd_allowed_object_type(
-	type_id INTEGER unsigned NOT NULL,
-	allowed_type_id INTEGER unsigned NOT NULL,
+	object_type_id BIGINT(20) unsigned NOT NULL,
+	allowed_object_type_id BIGINT(20) unsigned NOT NULL,
 
-	FOREIGN KEY(type_id) REFERENCES sd_object_type(id),
-	FOREIGN KEY(allowed_type_id) REFERENCES sd_object_type(id),
-	PRIMARY KEY(type_id, allowed_type_id)
+	FOREIGN KEY(object_type_id) REFERENCES sd_object_type(object_id),
+	FOREIGN KEY(allowed_object_type_id) REFERENCES sd_object_type(object_id),
+	PRIMARY KEY(object_type_id, allowed_object_type_id)
+);
+
+CREATE TABLE IF NOT EXISTS sd_forbidden_object_type(
+	object_type_id BIGINT(20) unsigned NOT NULL,
+	forbidden_object_type_id BIGINT(20) unsigned NOT NULL,
+
+	FOREIGN KEY(object_type_id) REFERENCES sd_object_type(object_id),
+	FOREIGN KEY(forbidden_object_type_id) REFERENCES sd_object_type(object_id),
+	PRIMARY KEY(object_type_id, forbidden_object_type_id)
 );
 
 CREATE TABLE IF NOT EXISTS sd_node (
 	id BIGINT(20) unsigned NOT NULL AUTO_INCREMENT,
 	parent_id BIGINT(20) unsigned NOT NULL,
-	role_id BIGINT(20) unsigned NOT NULL,
-	owner_id BIGINT(20) unsigned NOT NULL,
+	object_id BIGINT(20) unsigned NOT NULL,
+	object_type_id BIGINT(20) unsigned NOT NULL,
 
-	can_read BOOLEAN DEFAULT 0,
-	can_add BOOLEAN DEFAULT 0,
-	can_edit BOOLEAN DEFAULT 0,
-	can_delete BOOLEAN DEFAULT 0,
-	is_admin BOOLEAN DEFAULT 1,
+	inherit_permissions BOOLEAN NOT NULL DEFAULT 1,
 
 	PRIMARY KEY (id),
 	FOREIGN KEY (parent_id) REFERENCES sd_node(id),
-	FOREIGN KEY (role_id) REFERENCES sd_node(id),
-	FOREIGN KEY (owner_id) REFERENCES sd_node(id)
+	FOREIGN KEY (object_id) REFERENCES sd_object(id),
+	FOREIGN KEY (object_type_id) REFERENCES sd_object_type(object_id)
 );
 
 CREATE TABLE IF NOT EXISTS sd_version (
 	node_id BIGINT(20) unsigned NOT NULL,
-	lang VARCHAR(4),
+	lang VARCHAR(4) NOT NULL DEFAULT 'en',
 	revision_number INTEGER NOT NULL,
 	object_id BIGINT(20) unsigned NOT NULL,
 
-	revision_date TIMESTAMP,
+	revision_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
 	PRIMARY KEY (node_id, lang, revision_number, object_id),
 	FOREIGN KEY (node_id) REFERENCES sd_node(id),
@@ -56,15 +71,15 @@ CREATE TABLE IF NOT EXISTS sd_version (
 
 CREATE TABLE IF NOT EXISTS sd_object (
 	id BIGINT(20) unsigned NOT NULL AUTO_INCREMENT,
-	type_id INTEGER unsigned NOT NULL,
-
-	created_by BIGINT(20),
-	created_at TIMESTAMP,
-	modified_by BIGINT(20),
-	modified_at TIMESTAMP,
+	
+	created_by BIGINT(20) unsigned,
+	created_at TIMESTAMP NOT NULL,
+	modified_by BIGINT(20) unsigned,
+	modified_at TIMESTAMP NOT NULL,
 
 	PRIMARY KEY (id),
-	FOREIGN KEY (type_id) REFERENCES sd_object_type(id)
+	FOREIGN KEY (created_by) REFERENCES sd_user(object_id),
+	FOREIGN KEY (modified_by) REFERENCES sd_user(object_id)
 );
 
 CREATE TABLE IF NOT EXISTS sd_info(
@@ -83,14 +98,9 @@ CREATE TABLE IF NOT EXISTS sd_permission (
 	object_id BIGINT(20) unsigned NOT NULL,
 	entity_id BIGINT(20) unsigned NOT NULL,
 
-	is_inverse BOOLEAN DEFAULT 0,
-	can_read BOOLEAN DEFAULT 0,
-	can_add BOOLEAN DEFAULT 0,
-	can_edit BOOLEAN DEFAULT 0,
-	can_delete BOOLEAN DEFAULT 0,
-	is_admin BOOLEAN DEFAULT 0,
-
-	PRIMARY KEY (object_id),
+	permission_set TINYINT(3) unsigned NOT NULL DEFAULT 0,
+	
+	PRIMARY KEY (object_id, entity_id),
 	FOREIGN KEY (object_id) REFERENCES sd_object(id),
 	FOREIGN KEY (entity_id) REFERENCES sd_node(id)
 );
@@ -114,8 +124,8 @@ CREATE TABLE IF NOT EXISTS sd_session(
 	cookie VARCHAR(40),
 	remote_addr VARCHAR(40),
 	user_agent TEXT,
-	expire TIMESTAMP,
-	is_permanent BOOLEAN DEFAULT 0,
+	expire TIMESTAMP NOT NULL,
+	is_permanent BOOLEAN NOT NULL DEFAULT 0,
 
 	PRIMARY KEY (object_id),
 	FOREIGN KEY (object_id) REFERENCES sd_object(id)
@@ -133,3 +143,4 @@ CREATE TABLE sd_newobject(
 );
 */
 
+SET FOREIGN_KEY_CHECKS = 1;
